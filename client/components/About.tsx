46434a1d83150e8f5ede384881cf29bf3d434722
Yuekai Wu <yuekaixx@outlook.com>
2023-11-06 22:15:13 +1300

    add styles to about and thanks components

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -1,8 +1,20 @@
 function About() {
   return (
     <>
-      <h2 className="text-3xl font-bold">About</h2>
-      <p>Yuekai makes this web with love for Wenxiao and Mary.</p>
+      <div className="m-auto my-2 w-1/2 2xl:w-1/3 xl:w-1/3 lg:w-1/2 border-2 border-green-500">
+        <h2 className="text-3xl font-bold">About</h2>
+        <p className="text-left">
+          Hi, my name is Yuekai Wu. Welcome to my designed website, Prenatal
+          Treasures. This website aims to help new parents record their wish
+          list for their incoming babies. My wife, Wenxiao, and I are expecting
+          our first baby, Mary, in December, and this website is made with love
+          for Wenxiao and Mary.
+        </p>
+        <br />
+        <p className="text-left">
+          The website is for all parents who love their children!
+        </p>
+      </div>
     </>
   )
 }
--- client/components/SpecialThanks.tsx
+++ client/components/SpecialThanks.tsx
@@ -1,18 +1,26 @@
 function SpecialThanks() {
   return (
     <>
-      <h2 className="text-3xl font-bold">Special Thanks</h2>
-      <p className="align-text-top">
-        A very special thanks to{' '}
-        <a href="https://www.linkedin.com/in/udelduca">Rodolfo Usó Delduca</a>,
-        who provided the idea to create this lovely website. Rod, your idea for
-        this website inspired me.
-      </p>
-      <p>
-        Thanks as well to the entire cohort of mates and the facilitators in
-        Matai-2023 for their support and help during the lovely 17 weeks at Dev
-        Academy.
-      </p>
+      <div className="m-auto my-2 w-1/2 2xl:w-1/3 xl:w-1/3 lg:w-1/2 border-2 border-green-500">
+        <h2 className="text-3xl font-bold">Special Thanks</h2>
+        <p className="text-left">
+          A very special thanks to{' '}
+          <a
+            href="https://www.linkedin.com/in/udelduca"
+            className={'underline text-blue-600'}
+          >
+            Rodolfo Usó Delduca
+          </a>
+          , who provided the idea to create this lovely website. Rod, your idea
+          for this website inspired me.
+        </p>
+        <br />
+        <p className="text-left">
+          Thanks as well to the entire cohort of mates and the facilitators in
+          Matai-2023 for their support and help during the lovely 17 weeks at
+          Dev Academy Aotearoa.
+        </p>
+      </div>
     </>
   )
 }
